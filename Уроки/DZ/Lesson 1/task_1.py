ad18bb6b012aae714c39ffd7ac340e2ba9159006
Dudarevich Vasia <terminatori4ek@gmail.com>
2021-11-20 01:08:23 +0300

    Working version

--- Уроки/DZ/Lesson 1/task_1.py
+++ Уроки/DZ/Lesson 1/task_1.py
@@ -1,28 +1,29 @@
 import math
-
-second = int(input('Введите секунды:'))
-# список времени с количеством секунд в дне, часе, минуте соответственно
-time = [86400, 3600, 60]
-n = 0
-# инициализируем сравнение полученного числа со списком
-while n <= len(time) and second <= time[n]:
-    if n >= len(time):
-        print(second, 'сек.')
+while True:
+    try:
+        second = int(input('Введите секунды:'))
+    except ValueError:
+        print("Error! Это не число, попробуйте снова.")
     else:
-        n += 1
-if n == 1:
-    hour = math.floor(second/time[n])
-    minute = math.floor((second - hour * time[n])/time[n+1])
-    second = hour - minute * time[n+1]
-    print(hour, ' hour', minute, 'minute', second, 'second')
-else:
-    if n == 2:
-        minute = math.floor(second / time[n])
-        second = second - minute*time[n]
-        print(minute, 'minute', second, 'second')
-if n == 0:
-    days = math.floor(second/time[n])
-    hour = math.floor((second - days * time[n])/time[n+1])
-    minute = math.floor(((second - days * time[n]) - hour * time[n+1])/time[n+2])
-    second = second - (days * time[0] + hour * time[1] + minute * time[2])
-    print(days, 'days', hour, 'hour', minute, 'minute', second, 'second')
+        time = [86400, 3600, 60]
+        n = 0
+        if second >= time[n]:
+             days = math.floor(second / time[n])
+             hour = math.floor((second - days * time[n]) / time[n + 1])
+             minute = math.floor(((second - days * time[n]) - hour * time[n + 1]) / time[n + 2])
+             second = second - (days * time[0] + hour * time[1] + minute * time[2])
+             print(days, 'days', hour, 'hour', minute, 'minute', second, 'second')
+        else:
+         if second >= time[n+1]:
+                 hour = math.floor(second / time[n])
+                 minute = math.floor((second - hour * time[n]) / time[n + 1])
+                 second = hour - minute * time[n + 1]
+                 print(hour, ' hour', minute, 'minute', second, 'second')
+         else:
+          if second >= time[n+2]:
+                minute = math.floor(second / time[n])
+                second = second - minute * time[n]
+                print(minute, 'minute', second, 'second')
+          else:
+                print(second, 'сек.')
+        break
